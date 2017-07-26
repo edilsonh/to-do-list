b284a46484686a7e63e4c01bb9e4c568f733a507
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-26 16:18:42 -0400

    added body parser

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const mustacheExpress = require("mustache-express");
 app.engine("mustache", mustacheExpress());
 
 app.set("view engine", "mustache");
-app.set("views", __dirname + "views");
+app.set("views", __dirname + "/views");
+
+app.get("/", (req, res) =>{
+  res.render("index", {})
+});
 
 app.listen(3000);
